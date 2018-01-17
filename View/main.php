d706ca7af5a5bd85f626663ab012e123a423c25c
Kioup <mftrookie@gmail.com>
2018-01-17 12:36:26 +0100

    un peu de style

--- View/main.php
+++ View/main.php
@@ -8,13 +8,30 @@
 <link rel="stylesheet" type="text/css" href="./CSS/style.css">
 </head>
     
+<header>
+    <h1>Welcome to home shop</h1>
+    <div>
+        <input type="search" placeholder="rechercher...">
+        <a href="">Connexion</a>
+        <a href="">Inscription</a>
+    </div>
+</header>    
+<aside>
+    <div class="carte">
+        <h2>Categorie</h2>
+        <ul>
+            <li>Jouets</li>
+            <li>Materiel</li>
+            <li>Sports</li>
+        </ul>
+    </div>
+</aside>
 <body>
 
     <?php
         session_start();
         if(isset($_SESSION['user'])){                
             $user = $_SESSION["user"];
-            var_dump($user);
         }
     
         echo $info;
@@ -23,17 +40,21 @@
         }
         else {        
         ?>
+    
+        
+<!--
         <div class="Boite">
             <h2>Page Accueil</h2>
             <form>
-            <p><a href=<?php if(isset($_SESSION['user'])){echo 'index.php?ctrl=user&action=doLogin';
-        }else { echo 'index.php?ctrl=user&action=login';}?>>Connexion</a> ou <a href="#">Inscription</a></p>
+            <p><a href=<? php // if(isset($_SESSION['user'])){ // echo 'index.php?ctrl=user&action=doLogin';
+         // }else { echo 'index.php?ctrl=user&action=login';}?>>Connexion</a> ou <a href="#">Inscription</a></p>
                 <p class="Home"><a href="index.php">Accueil</a></p>
             <p><strong>TP Authentification et réseaux</strong></p>
             <p><a href="#">Gestion des utilisateurs</a></p>
             <p class="Home">Message</p>
             </form> 
         </div>
+-->
     <?php
         }
     ?>
